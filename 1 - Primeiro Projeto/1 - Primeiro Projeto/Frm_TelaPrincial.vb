08ff250a701bc1cf76a2ff762b3fddaad0c4668a
Vitor Martins <vitormartins.one@gmail.com>
2021-09-29 22:41:14 -0300

    Criação do projeto
    Estrutura dos componentes
    InitializeComponent()
    Estrutura da solução e dos projetos

---
+++ 1 - Primeiro Projeto/1 - Primeiro Projeto/Frm_TelaPrincial.vb
@@ -0,0 +1,14 @@
+﻿Public Class Frm_TelaPrincipal
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("ola mundo", MsgBoxStyle.AbortRetryIgnore)
+    End Sub
+End Class
---
+++ 1 - Primeiro Projeto/2 - Criando Variáveis/Frm_TelaPrincipal.vb
@@ -0,0 +1,10 @@
+﻿Public Class Frm_TelaPrincipal
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+    End Sub
+End Class
