419d10901a848d000721f4290c2a3feca695a2be
Vitor Martins <vitormartins.one@gmail.com>
2021-10-07 21:57:18 -0300

    Objetos
    Campos de um objeto;
    Valores padrões de campos de classes; Tipos de referência e tipos de valor;
    Atribuição de uma referência à uma variável;

---
+++ OrientacaoObjeto/OrientacaoObjeto/1 - Primeira Classe/Frm_Principal_01.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal_01
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lbl_Principal = New System.Windows.Forms.Label()
+        Me.Btn_Acao = New System.Windows.Forms.Button()
+        Me.Lbl_Saldo = New System.Windows.Forms.Label()
+        Me.Lbl_Incremento = New System.Windows.Forms.Label()
+        Me.Txt_Saldo = New System.Windows.Forms.TextBox()
+        Me.Txt_Incremento = New System.Windows.Forms.TextBox()
+        Me.Btn_Acao2 = New System.Windows.Forms.Button()
+        Me.Btn_ValRef = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Lbl_Principal
+        '
+        Me.Lbl_Principal.AutoSize = True
+        Me.Lbl_Principal.Location = New System.Drawing.Point(13, 13)
+        Me.Lbl_Principal.Name = "Lbl_Principal"
+        Me.Lbl_Principal.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Principal.TabIndex = 0
+        Me.Lbl_Principal.Text = "Label1"
+        '
+        'Btn_Acao
+        '
+        Me.Btn_Acao.Location = New System.Drawing.Point(15, 47)
+        Me.Btn_Acao.Name = "Btn_Acao"
+        Me.Btn_Acao.Size = New System.Drawing.Size(153, 36)
+        Me.Btn_Acao.TabIndex = 1
+        Me.Btn_Acao.Text = "Button1"
+        Me.Btn_Acao.UseVisualStyleBackColor = True
+        '
+        'Lbl_Saldo
+        '
+        Me.Lbl_Saldo.AutoSize = True
+        Me.Lbl_Saldo.Location = New System.Drawing.Point(13, 86)
+        Me.Lbl_Saldo.Name = "Lbl_Saldo"
+        Me.Lbl_Saldo.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Saldo.TabIndex = 2
+        Me.Lbl_Saldo.Text = "Label1"
+        '
+        'Lbl_Incremento
+        '
+        Me.Lbl_Incremento.AutoSize = True
+        Me.Lbl_Incremento.Location = New System.Drawing.Point(174, 85)
+        Me.Lbl_Incremento.Name = "Lbl_Incremento"
+        Me.Lbl_Incremento.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Incremento.TabIndex = 3
+        Me.Lbl_Incremento.Text = "Label1"
+        '
+        'Txt_Saldo
+        '
+        Me.Txt_Saldo.Location = New System.Drawing.Point(13, 106)
+        Me.Txt_Saldo.Name = "Txt_Saldo"
+        Me.Txt_Saldo.Size = New System.Drawing.Size(155, 23)
+        Me.Txt_Saldo.TabIndex = 4
+        '
+        'Txt_Incremento
+        '
+        Me.Txt_Incremento.Location = New System.Drawing.Point(174, 106)
+        Me.Txt_Incremento.Name = "Txt_Incremento"
+        Me.Txt_Incremento.Size = New System.Drawing.Size(154, 23)
+        Me.Txt_Incremento.TabIndex = 5
+        '
+        'Btn_Acao2
+        '
+        Me.Btn_Acao2.Location = New System.Drawing.Point(174, 47)
+        Me.Btn_Acao2.Name = "Btn_Acao2"
+        Me.Btn_Acao2.Size = New System.Drawing.Size(156, 36)
+        Me.Btn_Acao2.TabIndex = 6
+        Me.Btn_Acao2.Text = "Button1"
+        Me.Btn_Acao2.UseVisualStyleBackColor = True
+        '
+        'Btn_ValRef
+        '
+        Me.Btn_ValRef.Location = New System.Drawing.Point(383, 133)
+        Me.Btn_ValRef.Name = "Btn_ValRef"
+        Me.Btn_ValRef.Size = New System.Drawing.Size(152, 23)
+        Me.Btn_ValRef.TabIndex = 7
+        Me.Btn_ValRef.Text = "Button1"
+        Me.Btn_ValRef.UseVisualStyleBackColor = True
+        '
+        'Frm_Principal_01
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Btn_ValRef)
+        Me.Controls.Add(Me.Btn_Acao2)
+        Me.Controls.Add(Me.Txt_Incremento)
+        Me.Controls.Add(Me.Txt_Saldo)
+        Me.Controls.Add(Me.Lbl_Incremento)
+        Me.Controls.Add(Me.Lbl_Saldo)
+        Me.Controls.Add(Me.Btn_Acao)
+        Me.Controls.Add(Me.Lbl_Principal)
+        Me.Name = "Frm_Principal_01"
+        Me.Text = "Frm_Principal_01"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lbl_Principal As Label
+    Friend WithEvents Btn_Acao As Button
+    Friend WithEvents Lbl_Saldo As Label
+    Friend WithEvents Lbl_Incremento As Label
+    Friend WithEvents Txt_Saldo As TextBox
+    Friend WithEvents Txt_Incremento As TextBox
+    Friend WithEvents Btn_Acao2 As Button
+    Friend WithEvents Btn_ValRef As Button
+End Class
---
+++ OrientacaoObjeto/OrientacaoObjeto/1 - Primeira Classe/Frm_Principal_01.vb
@@ -0,0 +1,58 @@
+﻿Public Class Frm_Principal_01
+    Public Sub New()
+        InitializeComponent()
+        Me.Text = "1 - Primeira Classe"
+        Lbl_Principal.Text = "1 - Primeira Classe"
+        Btn_Acao.Text = "Conta Gabriela"
+        Lbl_Saldo.Text = "Saldo"
+        Lbl_Incremento.Text = "Incremento"
+        Btn_Acao2.Text = "Conta Bruno"
+        Btn_ValRef.Text = "Clique aqui"
+    End Sub
+
+    Private Sub Btn_Acao_Click(sender As Object, e As EventArgs) Handles Btn_Acao.Click
+        Dim ContaDaGabriela As New ContaCorrente
+
+        ContaDaGabriela.Titular = "Gabriela"
+        ContaDaGabriela.Agencia = 863
+        ContaDaGabriela.Conta = 863141
+        ContaDaGabriela.Saldo = Val(Txt_Saldo.Text)
+
+        MsgBox("O Saldo da " + ContaDaGabriela.Titular + " na C/C " +
+            ContaDaGabriela.Agencia.ToString + " - " + ContaDaGabriela.Conta.ToString +
+        " é " + ContaDaGabriela.Saldo.ToString)
+
+        Dim vIncremento As Double = Val(Txt_Incremento.Text)
+        ContaDaGabriela.Saldo += vIncremento
+
+        MsgBox("O Saldo da " + ContaDaGabriela.Titular + " na C/C " +
+            ContaDaGabriela.Agencia.ToString + " - " + ContaDaGabriela.Conta.ToString +
+        " é " + ContaDaGabriela.Saldo.ToString)
+    End Sub
+
+    Private Sub Btn_Acao2_Click(sender As Object, e As EventArgs) Handles Btn_Acao2.Click
+        Dim ContaDoBruno As New ContaCorrente
+
+        MsgBox(ContaDoBruno.Titular)
+        MsgBox(ContaDoBruno.Agencia)
+        MsgBox(ContaDoBruno.Conta)
+        MsgBox(ContaDoBruno.Saldo)
+
+        ContaDoBruno.Saldo += 100
+
+        MsgBox(ContaDoBruno.Saldo)
+    End Sub
+
+    Private Sub Btn_ValRef_Click(sender As Object, e As EventArgs) Handles Btn_ValRef.Click
+        Dim idade As Integer = 20
+        Dim idade2 As Integer
+
+        idade2 = idade
+
+        MsgBox("Valor da idade " + idade.ToString + " idade2 " + idade2.ToString)
+
+        idade = 30
+
+        MsgBox("Valor da idade " + idade.ToString + " idade2 " + idade2.ToString)
+    End Sub
+End Class
