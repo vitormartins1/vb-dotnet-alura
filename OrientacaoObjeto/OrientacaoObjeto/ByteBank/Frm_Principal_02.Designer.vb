2675c780524d9cdcc33be121c568dda55e38ee52
Vitor Martins <vitormartins.one@gmail.com>
2021-10-13 23:11:15 -0300

    namespaces

---
+++ OrientacaoObjeto/OrientacaoObjeto/ByteBank/Frm_Principal_02.Designer.vb
@@ -0,0 +1,377 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Principal_02
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Lbl_Principal = New System.Windows.Forms.Label()
+        Me.Grp_Gabriela = New System.Windows.Forms.GroupBox()
+        Me.Txt_Extrato_Gabriela = New System.Windows.Forms.TextBox()
+        Me.Txt_Resultado_Gabriela = New System.Windows.Forms.TextBox()
+        Me.Txt_Saldo_Gabriela = New System.Windows.Forms.TextBox()
+        Me.Txt_Valor_Gabriela = New System.Windows.Forms.TextBox()
+        Me.Txt_SaldoAtual_Gabriela = New System.Windows.Forms.TextBox()
+        Me.Lbl_Resultado_Gabriela = New System.Windows.Forms.Label()
+        Me.Lbl_Saldo_Gabriela = New System.Windows.Forms.Label()
+        Me.Lbl_Valor_Gabriela = New System.Windows.Forms.Label()
+        Me.Lbl_SaldoAtual_Gabriela = New System.Windows.Forms.Label()
+        Me.Lbl_BemVindo_Gabriela = New System.Windows.Forms.Label()
+        Me.Btn_Transferir_Gabriela = New System.Windows.Forms.Button()
+        Me.Btn_Sacar_Gabriela = New System.Windows.Forms.Button()
+        Me.Btn_Depositar_Gabriela = New System.Windows.Forms.Button()
+        Me.Grp_Bruno = New System.Windows.Forms.GroupBox()
+        Me.Txt_Extrato_Bruno = New System.Windows.Forms.TextBox()
+        Me.Txt_Resultado_Bruno = New System.Windows.Forms.TextBox()
+        Me.Txt_Saldo_Bruno = New System.Windows.Forms.TextBox()
+        Me.Txt_Valor_Bruno = New System.Windows.Forms.TextBox()
+        Me.Txt_SaldoAtual_Bruno = New System.Windows.Forms.TextBox()
+        Me.Lbl_Resultado_Bruno = New System.Windows.Forms.Label()
+        Me.Lbl_Saldo_Bruno = New System.Windows.Forms.Label()
+        Me.Lbl_Valor_Bruno = New System.Windows.Forms.Label()
+        Me.Lbl_SaldoAtual_Bruno = New System.Windows.Forms.Label()
+        Me.Lbl_BemVindo_Bruno = New System.Windows.Forms.Label()
+        Me.Btn_Transferir_Bruno = New System.Windows.Forms.Button()
+        Me.Btn_Sacar_Bruno = New System.Windows.Forms.Button()
+        Me.Btn_Depositar_Bruno = New System.Windows.Forms.Button()
+        Me.Grp_Gabriela.SuspendLayout()
+        Me.Grp_Bruno.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Lbl_Principal
+        '
+        Me.Lbl_Principal.AutoSize = True
+        Me.Lbl_Principal.Location = New System.Drawing.Point(13, 13)
+        Me.Lbl_Principal.Name = "Lbl_Principal"
+        Me.Lbl_Principal.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Principal.TabIndex = 0
+        Me.Lbl_Principal.Text = "Label1"
+        '
+        'Grp_Gabriela
+        '
+        Me.Grp_Gabriela.Controls.Add(Me.Txt_Extrato_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Txt_Resultado_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Txt_Saldo_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Txt_Valor_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Txt_SaldoAtual_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Lbl_Resultado_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Lbl_Saldo_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Lbl_Valor_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Lbl_SaldoAtual_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Lbl_BemVindo_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Btn_Transferir_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Btn_Sacar_Gabriela)
+        Me.Grp_Gabriela.Controls.Add(Me.Btn_Depositar_Gabriela)
+        Me.Grp_Gabriela.Location = New System.Drawing.Point(13, 32)
+        Me.Grp_Gabriela.Name = "Grp_Gabriela"
+        Me.Grp_Gabriela.Size = New System.Drawing.Size(406, 321)
+        Me.Grp_Gabriela.TabIndex = 1
+        Me.Grp_Gabriela.TabStop = False
+        Me.Grp_Gabriela.Text = "GroupBox1"
+        '
+        'Txt_Extrato_Gabriela
+        '
+        Me.Txt_Extrato_Gabriela.Location = New System.Drawing.Point(207, 70)
+        Me.Txt_Extrato_Gabriela.Multiline = True
+        Me.Txt_Extrato_Gabriela.Name = "Txt_Extrato_Gabriela"
+        Me.Txt_Extrato_Gabriela.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Txt_Extrato_Gabriela.Size = New System.Drawing.Size(192, 244)
+        Me.Txt_Extrato_Gabriela.TabIndex = 12
+        '
+        'Txt_Resultado_Gabriela
+        '
+        Me.Txt_Resultado_Gabriela.Location = New System.Drawing.Point(7, 291)
+        Me.Txt_Resultado_Gabriela.Name = "Txt_Resultado_Gabriela"
+        Me.Txt_Resultado_Gabriela.Size = New System.Drawing.Size(194, 23)
+        Me.Txt_Resultado_Gabriela.TabIndex = 11
+        '
+        'Txt_Saldo_Gabriela
+        '
+        Me.Txt_Saldo_Gabriela.Location = New System.Drawing.Point(7, 247)
+        Me.Txt_Saldo_Gabriela.Name = "Txt_Saldo_Gabriela"
+        Me.Txt_Saldo_Gabriela.Size = New System.Drawing.Size(194, 23)
+        Me.Txt_Saldo_Gabriela.TabIndex = 10
+        '
+        'Txt_Valor_Gabriela
+        '
+        Me.Txt_Valor_Gabriela.Location = New System.Drawing.Point(7, 116)
+        Me.Txt_Valor_Gabriela.Name = "Txt_Valor_Gabriela"
+        Me.Txt_Valor_Gabriela.Size = New System.Drawing.Size(194, 23)
+        Me.Txt_Valor_Gabriela.TabIndex = 9
+        '
+        'Txt_SaldoAtual_Gabriela
+        '
+        Me.Txt_SaldoAtual_Gabriela.Location = New System.Drawing.Point(7, 70)
+        Me.Txt_SaldoAtual_Gabriela.Name = "Txt_SaldoAtual_Gabriela"
+        Me.Txt_SaldoAtual_Gabriela.Size = New System.Drawing.Size(194, 23)
+        Me.Txt_SaldoAtual_Gabriela.TabIndex = 8
+        '
+        'Lbl_Resultado_Gabriela
+        '
+        Me.Lbl_Resultado_Gabriela.AutoSize = True
+        Me.Lbl_Resultado_Gabriela.Location = New System.Drawing.Point(7, 273)
+        Me.Lbl_Resultado_Gabriela.Name = "Lbl_Resultado_Gabriela"
+        Me.Lbl_Resultado_Gabriela.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Resultado_Gabriela.TabIndex = 7
+        Me.Lbl_Resultado_Gabriela.Text = "Label5"
+        '
+        'Lbl_Saldo_Gabriela
+        '
+        Me.Lbl_Saldo_Gabriela.AutoSize = True
+        Me.Lbl_Saldo_Gabriela.Location = New System.Drawing.Point(7, 229)
+        Me.Lbl_Saldo_Gabriela.Name = "Lbl_Saldo_Gabriela"
+        Me.Lbl_Saldo_Gabriela.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Saldo_Gabriela.TabIndex = 6
+        Me.Lbl_Saldo_Gabriela.Text = "Label4"
+        '
+        'Lbl_Valor_Gabriela
+        '
+        Me.Lbl_Valor_Gabriela.AutoSize = True
+        Me.Lbl_Valor_Gabriela.Location = New System.Drawing.Point(7, 96)
+        Me.Lbl_Valor_Gabriela.Name = "Lbl_Valor_Gabriela"
+        Me.Lbl_Valor_Gabriela.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Valor_Gabriela.TabIndex = 5
+        Me.Lbl_Valor_Gabriela.Text = "Label3"
+        '
+        'Lbl_SaldoAtual_Gabriela
+        '
+        Me.Lbl_SaldoAtual_Gabriela.AutoSize = True
+        Me.Lbl_SaldoAtual_Gabriela.Location = New System.Drawing.Point(7, 52)
+        Me.Lbl_SaldoAtual_Gabriela.Name = "Lbl_SaldoAtual_Gabriela"
+        Me.Lbl_SaldoAtual_Gabriela.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_SaldoAtual_Gabriela.TabIndex = 4
+        Me.Lbl_SaldoAtual_Gabriela.Text = "Label2"
+        '
+        'Lbl_BemVindo_Gabriela
+        '
+        Me.Lbl_BemVindo_Gabriela.AutoSize = True
+        Me.Lbl_BemVindo_Gabriela.Location = New System.Drawing.Point(7, 23)
+        Me.Lbl_BemVindo_Gabriela.Name = "Lbl_BemVindo_Gabriela"
+        Me.Lbl_BemVindo_Gabriela.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_BemVindo_Gabriela.TabIndex = 3
+        Me.Lbl_BemVindo_Gabriela.Text = "Label1"
+        '
+        'Btn_Transferir_Gabriela
+        '
+        Me.Btn_Transferir_Gabriela.Location = New System.Drawing.Point(6, 203)
+        Me.Btn_Transferir_Gabriela.Name = "Btn_Transferir_Gabriela"
+        Me.Btn_Transferir_Gabriela.Size = New System.Drawing.Size(195, 23)
+        Me.Btn_Transferir_Gabriela.TabIndex = 2
+        Me.Btn_Transferir_Gabriela.Text = "Button3"
+        Me.Btn_Transferir_Gabriela.UseVisualStyleBackColor = True
+        '
+        'Btn_Sacar_Gabriela
+        '
+        Me.Btn_Sacar_Gabriela.Location = New System.Drawing.Point(6, 174)
+        Me.Btn_Sacar_Gabriela.Name = "Btn_Sacar_Gabriela"
+        Me.Btn_Sacar_Gabriela.Size = New System.Drawing.Size(195, 23)
+        Me.Btn_Sacar_Gabriela.TabIndex = 1
+        Me.Btn_Sacar_Gabriela.Text = "Button2"
+        Me.Btn_Sacar_Gabriela.UseVisualStyleBackColor = True
+        '
+        'Btn_Depositar_Gabriela
+        '
+        Me.Btn_Depositar_Gabriela.Location = New System.Drawing.Point(6, 145)
+        Me.Btn_Depositar_Gabriela.Name = "Btn_Depositar_Gabriela"
+        Me.Btn_Depositar_Gabriela.Size = New System.Drawing.Size(195, 23)
+        Me.Btn_Depositar_Gabriela.TabIndex = 0
+        Me.Btn_Depositar_Gabriela.Text = "Button1"
+        Me.Btn_Depositar_Gabriela.UseVisualStyleBackColor = True
+        '
+        'Grp_Bruno
+        '
+        Me.Grp_Bruno.Controls.Add(Me.Txt_Extrato_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Txt_Resultado_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Txt_Saldo_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Txt_Valor_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Txt_SaldoAtual_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Lbl_Resultado_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Lbl_Saldo_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Lbl_Valor_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Lbl_SaldoAtual_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Lbl_BemVindo_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Btn_Transferir_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Btn_Sacar_Bruno)
+        Me.Grp_Bruno.Controls.Add(Me.Btn_Depositar_Bruno)
+        Me.Grp_Bruno.Location = New System.Drawing.Point(425, 32)
+        Me.Grp_Bruno.Name = "Grp_Bruno"
+        Me.Grp_Bruno.Size = New System.Drawing.Size(423, 321)
+        Me.Grp_Bruno.TabIndex = 13
+        Me.Grp_Bruno.TabStop = False
+        Me.Grp_Bruno.Text = "GroupBox2"
+        '
+        'Txt_Extrato_Bruno
+        '
+        Me.Txt_Extrato_Bruno.Location = New System.Drawing.Point(224, 70)
+        Me.Txt_Extrato_Bruno.Multiline = True
+        Me.Txt_Extrato_Bruno.Name = "Txt_Extrato_Bruno"
+        Me.Txt_Extrato_Bruno.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.Txt_Extrato_Bruno.Size = New System.Drawing.Size(192, 244)
+        Me.Txt_Extrato_Bruno.TabIndex = 12
+        '
+        'Txt_Resultado_Bruno
+        '
+        Me.Txt_Resultado_Bruno.Location = New System.Drawing.Point(6, 291)
+        Me.Txt_Resultado_Bruno.Name = "Txt_Resultado_Bruno"
+        Me.Txt_Resultado_Bruno.Size = New System.Drawing.Size(212, 23)
+        Me.Txt_Resultado_Bruno.TabIndex = 11
+        '
+        'Txt_Saldo_Bruno
+        '
+        Me.Txt_Saldo_Bruno.Location = New System.Drawing.Point(6, 247)
+        Me.Txt_Saldo_Bruno.Name = "Txt_Saldo_Bruno"
+        Me.Txt_Saldo_Bruno.Size = New System.Drawing.Size(212, 23)
+        Me.Txt_Saldo_Bruno.TabIndex = 10
+        '
+        'Txt_Valor_Bruno
+        '
+        Me.Txt_Valor_Bruno.Location = New System.Drawing.Point(7, 116)
+        Me.Txt_Valor_Bruno.Name = "Txt_Valor_Bruno"
+        Me.Txt_Valor_Bruno.Size = New System.Drawing.Size(212, 23)
+        Me.Txt_Valor_Bruno.TabIndex = 9
+        '
+        'Txt_SaldoAtual_Bruno
+        '
+        Me.Txt_SaldoAtual_Bruno.Location = New System.Drawing.Point(6, 70)
+        Me.Txt_SaldoAtual_Bruno.Name = "Txt_SaldoAtual_Bruno"
+        Me.Txt_SaldoAtual_Bruno.Size = New System.Drawing.Size(212, 23)
+        Me.Txt_SaldoAtual_Bruno.TabIndex = 8
+        '
+        'Lbl_Resultado_Bruno
+        '
+        Me.Lbl_Resultado_Bruno.AutoSize = True
+        Me.Lbl_Resultado_Bruno.Location = New System.Drawing.Point(6, 273)
+        Me.Lbl_Resultado_Bruno.Name = "Lbl_Resultado_Bruno"
+        Me.Lbl_Resultado_Bruno.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Resultado_Bruno.TabIndex = 7
+        Me.Lbl_Resultado_Bruno.Text = "Label6"
+        '
+        'Lbl_Saldo_Bruno
+        '
+        Me.Lbl_Saldo_Bruno.AutoSize = True
+        Me.Lbl_Saldo_Bruno.Location = New System.Drawing.Point(6, 229)
+        Me.Lbl_Saldo_Bruno.Name = "Lbl_Saldo_Bruno"
+        Me.Lbl_Saldo_Bruno.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Saldo_Bruno.TabIndex = 6
+        Me.Lbl_Saldo_Bruno.Text = "Label7"
+        '
+        'Lbl_Valor_Bruno
+        '
+        Me.Lbl_Valor_Bruno.AutoSize = True
+        Me.Lbl_Valor_Bruno.Location = New System.Drawing.Point(6, 96)
+        Me.Lbl_Valor_Bruno.Name = "Lbl_Valor_Bruno"
+        Me.Lbl_Valor_Bruno.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_Valor_Bruno.TabIndex = 5
+        Me.Lbl_Valor_Bruno.Text = "Label8"
+        '
+        'Lbl_SaldoAtual_Bruno
+        '
+        Me.Lbl_SaldoAtual_Bruno.AutoSize = True
+        Me.Lbl_SaldoAtual_Bruno.Location = New System.Drawing.Point(6, 52)
+        Me.Lbl_SaldoAtual_Bruno.Name = "Lbl_SaldoAtual_Bruno"
+        Me.Lbl_SaldoAtual_Bruno.Size = New System.Drawing.Size(41, 15)
+        Me.Lbl_SaldoAtual_Bruno.TabIndex = 4
+        Me.Lbl_SaldoAtual_Bruno.Text = "Label9"
+        '
+        'Lbl_BemVindo_Bruno
+        '
+        Me.Lbl_BemVindo_Bruno.AutoSize = True
+        Me.Lbl_BemVindo_Bruno.Location = New System.Drawing.Point(6, 23)
+        Me.Lbl_BemVindo_Bruno.Name = "Lbl_BemVindo_Bruno"
+        Me.Lbl_BemVindo_Bruno.Size = New System.Drawing.Size(47, 15)
+        Me.Lbl_BemVindo_Bruno.TabIndex = 3
+        Me.Lbl_BemVindo_Bruno.Text = "Label10"
+        '
+        'Btn_Transferir_Bruno
+        '
+        Me.Btn_Transferir_Bruno.Location = New System.Drawing.Point(7, 203)
+        Me.Btn_Transferir_Bruno.Name = "Btn_Transferir_Bruno"
+        Me.Btn_Transferir_Bruno.Size = New System.Drawing.Size(211, 23)
+        Me.Btn_Transferir_Bruno.TabIndex = 2
+        Me.Btn_Transferir_Bruno.Text = "Button4"
+        Me.Btn_Transferir_Bruno.UseVisualStyleBackColor = True
+        '
+        'Btn_Sacar_Bruno
+        '
+        Me.Btn_Sacar_Bruno.Location = New System.Drawing.Point(7, 174)
+        Me.Btn_Sacar_Bruno.Name = "Btn_Sacar_Bruno"
+        Me.Btn_Sacar_Bruno.Size = New System.Drawing.Size(211, 23)
+        Me.Btn_Sacar_Bruno.TabIndex = 1
+        Me.Btn_Sacar_Bruno.Text = "Button5"
+        Me.Btn_Sacar_Bruno.UseVisualStyleBackColor = True
+        '
+        'Btn_Depositar_Bruno
+        '
+        Me.Btn_Depositar_Bruno.Location = New System.Drawing.Point(7, 145)
+        Me.Btn_Depositar_Bruno.Name = "Btn_Depositar_Bruno"
+        Me.Btn_Depositar_Bruno.Size = New System.Drawing.Size(211, 23)
+        Me.Btn_Depositar_Bruno.TabIndex = 0
+        Me.Btn_Depositar_Bruno.Text = "Button6"
+        Me.Btn_Depositar_Bruno.UseVisualStyleBackColor = True
+        '
+        'Frm_Principal_02
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(858, 359)
+        Me.Controls.Add(Me.Grp_Bruno)
+        Me.Controls.Add(Me.Grp_Gabriela)
+        Me.Controls.Add(Me.Lbl_Principal)
+        Me.Name = "Frm_Principal_02"
+        Me.Text = "Frm_Principal_02"
+        Me.Grp_Gabriela.ResumeLayout(False)
+        Me.Grp_Gabriela.PerformLayout()
+        Me.Grp_Bruno.ResumeLayout(False)
+        Me.Grp_Bruno.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Lbl_Principal As Label
+    Friend WithEvents Grp_Gabriela As GroupBox
+    Friend WithEvents Txt_Extrato_Gabriela As TextBox
+    Friend WithEvents Txt_Resultado_Gabriela As TextBox
+    Friend WithEvents Txt_Saldo_Gabriela As TextBox
+    Friend WithEvents Txt_Valor_Gabriela As TextBox
+    Friend WithEvents Txt_SaldoAtual_Gabriela As TextBox
+    Friend WithEvents Lbl_Resultado_Gabriela As Label
+    Friend WithEvents Lbl_Saldo_Gabriela As Label
+    Friend WithEvents Lbl_Valor_Gabriela As Label
+    Friend WithEvents Lbl_SaldoAtual_Gabriela As Label
+    Friend WithEvents Lbl_BemVindo_Gabriela As Label
+    Friend WithEvents Btn_Transferir_Gabriela As Button
+    Friend WithEvents Btn_Sacar_Gabriela As Button
+    Friend WithEvents Btn_Depositar_Gabriela As Button
+    Friend WithEvents Grp_Bruno As GroupBox
+    Friend WithEvents Txt_Extrato_Bruno As TextBox
+    Friend WithEvents Txt_Resultado_Bruno As TextBox
+    Friend WithEvents Txt_Saldo_Bruno As TextBox
+    Friend WithEvents Txt_Valor_Bruno As TextBox
+    Friend WithEvents Txt_SaldoAtual_Bruno As TextBox
+    Friend WithEvents Lbl_Resultado_Bruno As Label
+    Friend WithEvents Lbl_Saldo_Bruno As Label
+    Friend WithEvents Lbl_Valor_Bruno As Label
+    Friend WithEvents Lbl_SaldoAtual_Bruno As Label
+    Friend WithEvents Lbl_BemVindo_Bruno As Label
+    Friend WithEvents Btn_Transferir_Bruno As Button
+    Friend WithEvents Btn_Sacar_Bruno As Button
+    Friend WithEvents Btn_Depositar_Bruno As Button
+End Class
---
+++ OrientacaoObjeto/OrientacaoObjeto/ByteBank/Frm_Principal_02.vb
@@ -0,0 +1,179 @@
+﻿Public Class Frm_Principal_02
+
+    Dim ContaDaGabriela As New ContaCorrente
+    Dim ContaDoBruno As New ContaCorrente
+
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+        Me.Text = "2 - ByteBank"
+        Lbl_Principal.Text = "2 - ByteBank"
+        Grp_Gabriela.Text = "Gabriela"
+        Lbl_Valor_Gabriela.Text = "Valor a ser depositado/sacado"
+        Lbl_Saldo_Gabriela.Text = "Novo Saldo"
+        Lbl_Resultado_Gabriela.Text = "Resultado do saque"
+        Lbl_SaldoAtual_Gabriela.Text = "Saldo Atual"
+        Btn_Sacar_Gabriela.Text = "Efetuar Saque"
+        Btn_Depositar_Gabriela.Text = "Efetuar Depósito"
+        Btn_Transferir_Gabriela.Text = "Efetuar Transferencia"
+        Txt_SaldoAtual_Gabriela.ReadOnly = True
+        Txt_Saldo_Gabriela.ReadOnly = True
+        Txt_Resultado_Gabriela.ReadOnly = True
+        Txt_Extrato_Gabriela.ReadOnly = True
+
+        Grp_Bruno.Text = "Bruno"
+        Lbl_Valor_Bruno.Text = "Valor a ser depositado/sacado"
+        Lbl_Saldo_Bruno.Text = "Novo Saldo"
+        Lbl_Resultado_Bruno.Text = "Resultado do saque"
+        Lbl_SaldoAtual_Bruno.Text = "Saldo Atual"
+        Btn_Sacar_Bruno.Text = "Efetuar Saque"
+        Btn_Depositar_Bruno.Text = "Efetuar Depósito"
+        Btn_Transferir_Bruno.Text = "Efetuar Transferencia"
+        Txt_SaldoAtual_Bruno.ReadOnly = True
+        Txt_Saldo_Bruno.ReadOnly = True
+        Txt_Resultado_Bruno.ReadOnly = True
+        Txt_Extrato_Bruno.ReadOnly = True
+
+        ContaDaGabriela.Titular = "Gabriela"
+        ContaDaGabriela.Agencia = 863
+        ContaDaGabriela.Conta = 863141
+
+        Lbl_BemVindo_Gabriela.Text = "Bem Vindo " + ContaDaGabriela.Titular +
+            " Agência: " + ContaDaGabriela.Agencia.ToString +
+            " Conta Corrente: " + ContaDaGabriela.Conta.ToString
+        Txt_SaldoAtual_Gabriela.Text = ContaDaGabriela.Saldo.ToString
+
+        ContaDoBruno.Titular = "Bruno"
+        ContaDoBruno.Agencia = 863
+        ContaDoBruno.Conta = 863224
+
+        Lbl_BemVindo_Bruno.Text = "Bem Vindo " + ContaDoBruno.Titular +
+            " Agência: " + ContaDoBruno.Agencia.ToString +
+            " Conta Corrente: " + ContaDoBruno.Conta.ToString
+        Txt_SaldoAtual_Bruno.Text = ContaDoBruno.Saldo.ToString
+    End Sub
+
+    Private Sub Btn_Depositar_Gabriela_Click(sender As Object, e As EventArgs) Handles Btn_Depositar_Gabriela.Click
+        Txt_Resultado_Gabriela.Text = ""
+        Txt_Saldo_Gabriela.Text = ""
+
+        Dim ValorDepositar As Double = Val(Txt_Valor_Gabriela.Text)
+        ContaDaGabriela.Depositar(ValorDepositar)
+
+        Txt_Saldo_Gabriela.Text = ContaDaGabriela.Saldo.ToString
+        Txt_Resultado_Gabriela.Text = "Depósito efetuado com sucesso"
+        Txt_SaldoAtual_Gabriela.Text = Txt_Saldo_Gabriela.Text
+        ContaDaGabriela.Extrato += Now.ToString + " Depósito de " + ValorDepositar.ToString +
+            " Saldo " + ContaDaGabriela.Saldo.ToString + vbCrLf
+        Txt_Extrato_Gabriela.Text = ContaDaGabriela.Extrato
+    End Sub
+
+    Private Sub Btn_Sacar_Gabriela_Click(sender As Object, e As EventArgs) Handles Btn_Sacar_Gabriela.Click
+        Txt_Resultado_Gabriela.Text = ""
+        Txt_Saldo_Gabriela.Text = ""
+
+        Dim ValorSacar As Double = Val(Txt_Valor_Gabriela.Text)
+        Dim RetornoSaque As Boolean = ContaDaGabriela.Sacar(ValorSacar)
+
+        If RetornoSaque = False Then
+            Txt_Resultado_Gabriela.Text = "Saque não é possível ser feito"
+        Else
+            Txt_Saldo_Gabriela.Text = ContaDaGabriela.Saldo.ToString
+            Txt_Resultado_Gabriela.Text = "Saque efetuado com sucesso"
+            Txt_SaldoAtual_Gabriela.Text = Txt_Saldo_Gabriela.Text
+            ContaDaGabriela.Extrato += Now.ToString + " Saque de " + ValorSacar.ToString +
+                " Saldo " + ContaDaGabriela.Saldo.ToString + vbCrLf
+            Txt_Extrato_Gabriela.Text = ContaDaGabriela.Extrato
+
+        End If
+    End Sub
+
+    Private Sub Btn_Transferir_Gabriela_Click(sender As Object, e As EventArgs) Handles Btn_Transferir_Gabriela.Click
+        Txt_Resultado_Gabriela.Text = ""
+        Txt_Saldo_Gabriela.Text = ""
+
+        Dim ValorTransferir As Double = Val(Txt_Valor_Gabriela.Text)
+        Dim RetornoTransferencia As Boolean = ContaDaGabriela.Transferir(ValorTransferir, ContaDoBruno)
+
+        If RetornoTransferencia = False Then
+            Txt_Resultado_Gabriela.Text = "Transferencia não é possível ser feito"
+        Else
+            Txt_Saldo_Gabriela.Text = ContaDaGabriela.Saldo.ToString
+            Txt_Saldo_Bruno.Text = ContaDoBruno.Saldo.ToString
+
+            Txt_Resultado_Gabriela.Text = "Transferencia efetuada com sucesso"
+            Txt_SaldoAtual_Gabriela.Text = Txt_Saldo_Gabriela.Text
+            Txt_SaldoAtual_Bruno.Text = Txt_Saldo_Bruno.Text
+
+            ContaDaGabriela.Extrato += Now.ToString + " Transferencia de " + ValorTransferir.ToString +
+                " Saldo " + ContaDaGabriela.Saldo.ToString + vbCrLf
+            ContaDoBruno.Extrato += Now.ToString + " Transferencia de " + ValorTransferir.ToString +
+                " Saldo " + ContaDoBruno.Saldo.ToString + vbCrLf
+
+            Txt_Extrato_Gabriela.Text = ContaDaGabriela.Extrato
+            Txt_Extrato_Bruno.Text = ContaDoBruno.Extrato
+        End If
+    End Sub
+
+    Private Sub Btn_Depositar_Bruno_Click(sender As Object, e As EventArgs) Handles Btn_Depositar_Bruno.Click
+        Txt_Resultado_Bruno.Text = ""
+        Txt_Saldo_Bruno.Text = ""
+
+        Dim ValorDepositar As Double = Val(Txt_Valor_Bruno.Text)
+        ContaDoBruno.Depositar(ValorDepositar)
+
+        Txt_Saldo_Bruno.Text = ContaDoBruno.Saldo.ToString
+        Txt_Resultado_Bruno.Text = "Depósito efetuado com sucesso"
+        Txt_SaldoAtual_Bruno.Text = Txt_Saldo_Bruno.Text
+        ContaDoBruno.Extrato += Now.ToString + " Depósito de " + ValorDepositar.ToString +
+            " Saldo " + ContaDoBruno.Saldo.ToString + vbCrLf
+        Txt_Extrato_Bruno.Text = ContaDoBruno.Extrato
+    End Sub
+
+    Private Sub Btn_Sacar_Bruno_Click(sender As Object, e As EventArgs) Handles Btn_Sacar_Bruno.Click
+        Txt_Resultado_Bruno.Text = ""
+        Txt_Saldo_Bruno.Text = ""
+
+        Dim ValorSacar As Double = Val(Txt_Valor_Bruno.Text)
+        Dim RetornoSaque As Boolean = ContaDoBruno.Sacar(ValorSacar)
+
+        If RetornoSaque = False Then
+            Txt_Resultado_Bruno.Text = "Saque não é possível ser feito"
+        Else
+            Txt_Saldo_Bruno.Text = ContaDoBruno.Saldo.ToString
+            Txt_Resultado_Bruno.Text = "Saque efetuado com sucesso"
+            Txt_SaldoAtual_Bruno.Text = Txt_Saldo_Bruno.Text
+            ContaDoBruno.Extrato += Now.ToString + " Saque de " + ValorSacar.ToString +
+                " Saldo " + ContaDoBruno.Saldo.ToString + vbCrLf
+            Txt_Extrato_Bruno.Text = ContaDoBruno.Extrato
+        End If
+    End Sub
+
+    Private Sub Btn_Transferir_Bruno_Click(sender As Object, e As EventArgs) Handles Btn_Transferir_Bruno.Click
+        Txt_Resultado_Bruno.Text = ""
+        Txt_Saldo_Bruno.Text = ""
+
+        Dim ValorTransferir As Double = Val(Txt_Valor_Bruno.Text)
+        Dim RetornoTransferencia As Boolean = ContaDoBruno.Transferir(ValorTransferir, ContaDaGabriela)
+
+        If RetornoTransferencia = False Then
+            Txt_Resultado_Bruno.Text = "Transferencia não é possível ser feito"
+        Else
+            Txt_Saldo_Gabriela.Text = ContaDaGabriela.Saldo.ToString
+            Txt_Saldo_Bruno.Text = ContaDoBruno.Saldo.ToString
+
+            Txt_Resultado_Bruno.Text = "Transferencia efetuada com sucesso"
+            Txt_SaldoAtual_Gabriela.Text = Txt_Saldo_Gabriela.Text
+            Txt_SaldoAtual_Bruno.Text = Txt_Saldo_Bruno.Text
+
+            ContaDaGabriela.Extrato += Now.ToString + " Transferencia de " + ValorTransferir.ToString +
+                " Saldo " + ContaDaGabriela.Saldo.ToString + vbCrLf
+            ContaDoBruno.Extrato += Now.ToString + " Transferencia de " + ValorTransferir.ToString +
+                " Saldo " + ContaDoBruno.Saldo.ToString + vbCrLf
+
+            Txt_Extrato_Gabriela.Text = ContaDaGabriela.Extrato
+            Txt_Extrato_Bruno.Text = ContaDoBruno.Extrato
+        End If
+    End Sub
+End Class
